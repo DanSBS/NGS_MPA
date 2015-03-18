a5698057761278a4146b20c1142b0172b159d50c
DanSBS <daniel.samarov@gmail.com>
2015-03-18 10:00:53 -0400

    Added new data set

--- NGS_MPA/R/analysis.R
+++ NGS_MPA/R/analysis.R
@@ -5,22 +5,28 @@
 library(PMA)
 
 d <- dir('data')
+d <- d[d!='PlatGen_ROC_wclus.csv']
 nf <- length(d)
 
 X <- vector('list',nf)
+
 for(i in 1:nf){
 	f <- paste('data/',d[i],sep='')
 	print(f)
 	tmp <- read.csv(f,as.is=TRUE,
 			stringsAsFactors=FALSE)
+	
 	if(d[i] == "PlatGen_ROC.csv")
 		tmp <- tmp[,names(tmp) != 'R_Ins_10_percentile']
 	s <- 1:4
 	nc <- ncol(tmp)
 	s <- c(s, (nc-10):nc)
-	xtmp <- scale(apply(as.matrix(tmp[,-s]), 2, asinh))
+	xtmp <- scale(apply(as.matrix(tmp[,-s]), 2, asinh))[tmp$GROUP %in% c(1:7), ]
 	X[[i]] <- xtmp
 }
+
+clus <- read.csv('data/PlatGen_ROC_wclus.csv',as.is=TRUE,
+		stringsAsFactors=FALSE)$Cluster
 
 ## Run sparse CCA
 perm <- MultiCCA.permute(X)
@@ -39,11 +45,11 @@ cols[tmp$GROUP == 13] <- 'red'
 highlight <- rep(FALSE, nrow(tmp))
 highlight[tmp$GROUP %in% c(1,6,13)] <- TRUE
 
-
+i=2
 for(i in 1:nf){
 	png(paste('plots/sparse_cca',i,'.png',sep=''), height = 800, width = 800)
-	plotcca(plist[[i]][,1:3],cca$ws[[i]][,1:3],3,
-			colnames(X[[i]]),pch=19,cex=0.5, col = cols,
-			pltname = d[i], highlight = highlight, cex.main = 2)
+	plotcca(plist[[i]],cca$ws[[i]],5,
+			colnames(X[[i]]),pch=19,cex=0.5, col = clus,
+			pltname = d[i], highlight = NULL, cex.main = 2)
 	dev.off()
 }
